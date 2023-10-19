ffdeda954c909df498e4ef874812ea8362ad9f14
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 09:47:38 +0300

    efef

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [ratioChange, setRatioChange] = useState (1);
+  function handleRatioChange(event){
+    setRatioChange(event.target.value);
+  }
   return (
     <div>
+      <input type="number" value = {ratioChange} onCange = {handleRatioChange}/> 
       <p>Nospiests {count} Reizes</p>
       <button onClick={() => setCount(count + 1)}>
         Spied +1
